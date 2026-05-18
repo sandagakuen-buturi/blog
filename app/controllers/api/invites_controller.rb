f8083a8d22ee44286be4f84d813237ab7310e61f
tanahiro2010 <herentongkegu087@gmail.com>
2026-05-19 07:45:42 +0900

    feat: invite取得エンドポイントを実装

--- app/controllers/api/invites_controller.rb
+++ app/controllers/api/invites_controller.rb
@@ -1,6 +1,14 @@
 class Api::InvitesController < ApiApplicationController
   def show
-    invite = Invite
+    token = params[:id]
+    invite = Invite.find_valid_invite(token)
+    if invite.nil?
+      render json: ApiApplicationHelper::Response.not_found(message: "Invalid or expired invite token", data: {
+        token: token
+      }), status: :not_found
+      return
+    end
+
     render json: ApiApplicationHelper::Response.ok(message: "#{id} hello")
   end
 
