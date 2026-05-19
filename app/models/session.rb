28f262558188171474755792eb1ba620cf10b534
tanahiro2010 <herentongkegu087@gmail.com>
2026-05-20 07:38:29 +0900

    feat: セッション取得用の静的ヘルパー関数を作成

--- app/models/session.rb
+++ app/models/session.rb
@@ -2,5 +2,8 @@ class Session < ApiApplicationController
   scope :active, -> { where("expire_at > ?", Time.current) }
 
   class << self
+    def find_active_by_key(key)
+      active.find_by(key: key)
+    end
   end
 end
