04f47575d727b9f5b21892770f62d220c2045159
tanahiro2010 <herentongkegu087@gmail.com>
2026-05-20 19:21:33 +0900

    feat: ミドルウェアの雛形を追加

--- app/controllers/api_application_controller.rb
+++ app/controllers/api_application_controller.rb
@@ -2,4 +2,25 @@
 
 class ApiApplicationController < ActionController::API
   include ActionController::MimeResponds
+  before_action :auth_middleware
+
+  private
+  def auth_middleware
+    auth_header = request.headers["Authorization"]
+    unless auth_header.present? && auth_header.start_with?("Bearer ")
+      render json: ApiApplicationHelper::Response.unauthorized, status: :unauthorized
+    end
+
+    token = auth_header.split(" ").last
+    session = Session.find_active_by_key(token)
+    unless session
+      render json: ApiApplicationHelper::Response.unauthorized, status: :unauthorized
+    end
+
+    user = Auth.find_by(username: session.user)
+    unless user
+      render json: ApiApplicationHelper::Response.unauthorized, status: :unauthorized
+    end
+    @current_user = user
+  end
 end
