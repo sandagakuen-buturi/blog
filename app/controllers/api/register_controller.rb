c311e1f63a3b4c8dc5e8ec9aab1c345650d0565f
tanahiro2010 <herentongkegu087@gmail.com>
2026-05-27 15:58:23 +0900

    feat(register): APIでの登録エンドポイントを追加

--- app/controllers/api/register_controller.rb
+++ app/controllers/api/register_controller.rb
@@ -1,4 +1,4 @@
-class Public::Api::RegisterController < ApiApplicationController
+class Api::RegisterController < ApiApplicationController
   def create
     email = params[:email]
     if Invite.exists?(email: email)
@@ -7,14 +7,21 @@ class Public::Api::RegisterController < ApiApplicationController
       return
     end
 
-    invite = Invite.create_invite(email)
-
-    result = InviteMailer.with(invite).invite_email
-    if result.is_a?(Net::HTTPOK)
-      res = ApiApplicationHelper::Response.success(message: "Invite email sent successfully")
-      render json: res
-    else
-      res = ApiApplicationHelper::Response.error(message: "Failed to send invite email")
+    begin
+      invite = Invite.create_invite(email)
+      result = InviteMailer.with(invite).invite_email
+      if result.is_a?(Net::HTTPOK)
+        res = ApiApplicationHelper::Response.success(message: "Invite email sent successfully")
+        render json: res
+      else
+        res = ApiApplicationHelper::Response.error(message: "Failed to send invite email")
+        render json: res, status: :internal_server_error
+      end
+    rescue Error => e
+      p e
+      res = ApiApplicationHelper::Response.error(message: "Failed to create invite", data: {
+        error: e.message
+      })
       render json: res, status: :internal_server_error
     end
   end
